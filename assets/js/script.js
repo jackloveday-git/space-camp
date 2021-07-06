160b006c2a53ece3e25092415240e71b7a0a0c40
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-07-06 16:01:04 -0400

    added visible and hidden functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,63 +78,132 @@ fetch(marsRoverAPI)
     console.log(randomPic);
     //create img element to hold picture
     var curiosityPic = $("<img></img>").attr("src", randomPic.img_src);
-    //append img div to parent el
-    parentDiv.append(curiosityPic);
+
+    //create div that will hold the description
+    var divForText = $("<div></div>").attr("id", "extra1").addClass("hidden");
+    //append img div to parent el //append div to parentDiv
+    parentDiv.append(curiosityPic, divForText);
+
+    var cameraName = $("<h3></h3>").text(
+      "Camera Name: " + randomPic.camera.full_name
+    );
+    var pictureDate = $("<h3></h3>").text(
+      "Earth Date: " + randomPic.earth_date
+    );
+
+    var roverName = $("<h3></h3>").text("Rover Name: " + randomPic.rover.name);
+
+    var roverLandDate = $("<h3></h3>").text(
+      "Date Landed: " + randomPic.rover.landing_date
+    );
+    divForText.append(cameraName, pictureDate, roverName, roverLandDate);
   });
 
 //---------------people in space API--------------
-var peopleInSpaceAPI = "http://api.open-notify.org/astros.json";
 
-//fetch request using stored API variable
-fetch(peopleInSpaceAPI)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log(response);
-    //reference parent2 DIV from HTML file
-    var parentDiv = $("#parent2");
-    //display total people
-    var divForTotal = $("<div></div>");
-    //text for total people in space
-    var totalPeople = $("<h2></h2>").text(
-      "Number of People in Space: " + response.number
-    );
-    //append text to new div
-    divForTotal.append(totalPeople);
+var peopleInSpace = function () {
+  var peopleInSpaceAPI = "http://api.open-notify.org/astros.json";
 
-    //append new div to parent el
-    parentDiv.append(divForTotal);
+  //fetch request using stored API variable
+  fetch(peopleInSpaceAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      //reference parent2 DIV from HTML file
+      var parentDiv = $("#parent2");
+      //display total people
+      var divForTotal = $("<div></div>");
+      //text for total people in space
+      var totalPeople = $("<h2></h2>").text(
+        "Number of People in Space: " + response.number
+      );
+      //append text to new div
+      divForTotal.append(totalPeople);
 
-    //store array of people in space to iterate through later
-    var peopleArray = response.people;
-    //title for ISS
-    var issTitle = $("<h2></h2>").text(
-      "Craft: International Space Station (ISS)"
-    );
-    //unordered list for all ISS astronauts
-    var ulISS = $("<ul></ul>");
-    //append ul to issTitle
-    issTitle.append(ulISS);
-    //title for tiangong space station
-    var tiangongTitle = $("<h2></h2>").text("Craft: Tiangong Space Station");
-    //unordered list for all tiangong astronauts
-    var ulTiangong = $("<ul></ul>");
-    //append ul to tiangongTitle
-    tiangongTitle.append(ulTiangong);
+      //append new div to parent el
+      parentDiv.append(divForTotal);
 
-    for (let i = 0; i < peopleArray.length; i++) {
-      //if statement --- if craft === to ISS // if statement --- if craft === to Tiangong to place astronauts in correct list
-      if (response.people[i].craft === "ISS") {
-        var listText = $("<li></li>").text(response.people[i].name);
+      //create div to hold information
+      var extraDiv = $("<div></div>").attr("id", "extra2").addClass("hidden");
+      //append new div to parent el
+      parentDiv.append(divForTotal, extraDiv);
+
+      //store array of people in space to iterate through later
+      var peopleArray = response.people;
+      //title for ISS
+      var issTitle = $("<h2></h2>").text(
+        "Craft: International Space Station (ISS)"
+      );
+      //unordered list for all ISS astronauts
+      var ulISS = $("<ul></ul>");
+      //append ul to issTitle
+      issTitle.append(ulISS);
+      //title for tiangong space station
+      var tiangongTitle = $("<h2></h2>").text("Craft: Tiangong Space Station");
+      //unordered list for all tiangong astronauts
+      var ulTiangong = $("<ul></ul>");
+      //append ul to tiangongTitle
+      tiangongTitle.append(ulTiangong);
+
+      for (let i = 0; i < peopleArray.length; i++) {
+        //if statement --- if craft === to ISS // if statement --- if craft === to Tiangong to place astronauts in correct list
+        if (response.people[i].craft === "ISS") {
+          var listText = $("<li></li>").text(response.people[i].name);
+        }
+        if (response.people[i].craft === "Tiangong") {
+          var listText2 = $("<li></li>").text(response.people[i].name);
+        }
+        //append li elements to corresponding ul element
+        ulISS.append(listText);
+        ulTiangong.append(listText2);
       }
-      if (response.people[i].craft === "Tiangong") {
-        var listText2 = $("<li></li>").text(response.people[i].name);
-      }
-      //append li elements to corresponding ul element
-      ulISS.append(listText);
-      ulTiangong.append(listText2);
+      //append titles to parent div
+      extraDiv.append(issTitle, tiangongTitle);
+    });
+};
+
+peopleInSpace();
+
+//set to not visible to start
+var isVisible = false;
+//when clicked, more information is loaded in parent element 1
+var hideAndVisible1 = function () {
+  $(".parent1").on("click", function () {
+    //if element is visible, hide if hidden, make visible
+    if (isVisible === true) {
+      var extraDiv = $("#extra1");
+      extraDiv.addClass("hidden");
+      return (isVisible = false);
+    } else {
+      var extraDiv = $("#extra1");
+      extraDiv.removeClass("hidden");
+      return (isVisible = true);
     }
-    //append titles to parent div
-    parentDiv.append(issTitle, tiangongTitle);
   });
+};
+
+//show and hide mars rover information
+hideAndVisible1();
+
+//set to not visible to start
+var isVisible = false;
+//when clicked, more information is loaded in parent el 2
+var hideAndVisible2 = function () {
+  $(".parent2").on("click", function () {
+    //if element is visible, hide if hidden, make visible
+    if (isVisible === true) {
+      var extraDiv = $("#extra2");
+      extraDiv.addClass("hidden");
+      return (isVisible = false);
+    } else {
+      var extraDiv = $("#extra2");
+      extraDiv.removeClass("hidden");
+      return (isVisible = true);
+    }
+  });
+};
+
+//show and hide number of people in space information
+hideAndVisible2();
